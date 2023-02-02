aa413e57e7e40d9f52b62ed78440acc39dbc8d59
MURAOKA Taro <koron.kaoriya@gmail.com>
2023-02-03 00:20:46 +0900

    curve out usb_mouse library (WIP)

---
+++ usb_mouse/include/usb/mouse.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void usb_mouse_task(uint64_t now);
---
+++ usb_mouse/mouse.c
@@ -0,0 +1,26 @@
+#include "tusb.h"
+
+#include "usb/mouse.h"
+
+// add16 adds two int16_t with clipping.
+static int16_t add16(int16_t a, int16_t b) {
+    int16_t r = a + b;
+    if (a >= 0 && b >= 0 && r < 0) {
+        r = 32767;
+    } else if (a < 0 && b < 0 && r >= 0) {
+        r = -32768;
+    }
+    return r;
+}
+
+// clip2int8 clips an integer fit into int8_t.
+static inline int8_t clip2int8(int16_t v) {
+    return (v) < -127 ? -127 : (v) > 127 ? 127 : (int8_t)v;
+}
+
+void usb_mouse_task(uint64_t now) {
+    if (!tud_hid_n_ready(ITF_NUM_HID)) {
+        return;
+    }
+    // TODO:
+}
